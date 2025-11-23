32f2abd7d0cbb0f3864ea5690d0b8787e6b0d2d5
Charis <Charisnnadi15@gmail.com>
2025-11-23 13:12:54 +0100

    Desired state = 0

--- infra/main.tf
+++ infra/main.tf
@@ -137,7 +137,7 @@ resource "aws_ecs_service" "app_service" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app_task.arn
   launch_type     = "FARGATE"
-  desired_count   = 1
+  desired_count   = 0
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
